f21cfe788f0f69900d885387558129640a4a9e6c
Rekkin0 <korzenmichal5@gmail.com>
2022-10-17 21:54:12 +0200

    comitussy

--- lab01/arrays.cpp
+++ lab01/arrays.cpp
@@ -18,21 +18,21 @@ void alloc_array_fill_34(int size) {
     delete[] array;
 }
 
-bool alloc_array_2D(int**& array, int sizeX, int sizeY) {
+bool alloc_array_2D(int*** array, int sizeX, int sizeY) {
 
     if (sizeX <= 0 || sizeY <= 0) {
         std::cout << "Nieprawidłowy rozmiar tablicy." << std::endl;
         return false;
     }
 
-    array = new int*[sizeX];
+    *array = new int*[sizeX];
     for (int i = 0; i < sizeX; i++)
-        array[i] = new int[sizeY];
+        (*array)[i] = new int[sizeY];
 
     return true;
 }
 
-bool dealloc_array_2D(int**& array, int sizeX) {
+bool dealloc_array_2D(int*** array, int sizeX) {
 
     if (sizeX <= 0) {
         std::cout << "Podano nieprawidłowy rozmiar." << std::endl;
@@ -40,9 +40,9 @@ bool dealloc_array_2D(int**& array, int sizeX) {
     }
 
     for (int i = 0; i < sizeX; i++)
-        delete[] array[i];
+        delete[] (*array)[i];
     
-    delete[] array;
+    delete[] *array;
     return true;
 }
 
@@ -50,8 +50,8 @@ bool dealloc_array_2D(int**& array, int sizeX) {
 int main() {
 
     int** array;
-    bool canDealloc = alloc_array_2D(array, 5, 3);
-    if (canDealloc) dealloc_array_2D(array, 5);
+    bool canDealloc = alloc_array_2D(&array, 5, 3);
+    if (canDealloc) dealloc_array_2D(&array, 5);
 
     return 0;
 }
